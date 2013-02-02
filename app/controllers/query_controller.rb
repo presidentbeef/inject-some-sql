8b4441acfcf7dc92b2d3f446f0e1664b3f422a7d
Justin Collins <justin@presidentbeef.com>
2013-02-02 12:10:06 -0800

    Make it all dynamic

--- app/controllers/query_controller.rb
+++ app/controllers/query_controller.rb
@@ -3,78 +3,12 @@ class QueryController < ApplicationController
     @queries = Queries
   end
 
-  #Calculate methods (average, count, maximum, minimum, sum, pluck(?))
-  #are shortcuts for calculate. calculate takes any SQL you would like
-  #for the second parameter which selects a column.
-  def calculate
-    show Order.calculate(:sum, params[:column])
-  end
-
-  #Conditions strings are not escaped.
-  def conditions_string
-    show User.first(:conditions => "name = '#{params[:name]}'")
-  end
-
-  def exists
-  end
-
-  #From options are not escaped.
-  def from_option
-    show User.all(:from => params[:from]).where(:admin => false)
-  end
-
-  #Group options are not escaped.
-  def group_option
-    show User.find(:all, :group => params[:group], :where => { :admin => false})
-  end
-
-  #Having is not escaped.
-  def having
-    show Order.where(:customer_id => current_user).group(:customer_id).having("total > #{params[:total]}")
-  end
-
-  #Having options are not escaped.
-  def having_option
-    show Order.all(:conditions => { :customer_id => current_user }, :group => :customer_id, :having => "total > #{params[:total]}")
-  end
-
-  #Joins are not escaped.
-  def joins
-    show Order.where(:customer_id => current_user).joins(params[:table])
-  end
-
-  #Lock option is not escaped, but I think sqlite doesn't support it?
-  def lock_option
-  end
-
-  #Order is not at all escaped.
-  def order
-    show User.all.order("#{params[:column]} #{params[:sort]}")
-  end
-
-  #Order options are not escaped.
-  def order_option
-    show User.all(:order => "name #{params[:sort]}")
-  end
-
-  #Select options are definitely not escaped.
-  def select_option
-    show User.first(:conditions => { :name => params[:name], :password => params[:password] }, :select => params[:column])
-  end
-
-  #Where is not escaped unless explicitly parameterized.
-  def where
-    show User.where("name = '#{params[:name]}' AND password = '#{params[:password]}'")
-  end
-
-  #Update_all takes straight up SQL.
-  def update_all
-    show User.update_all(params[:update])
-  end
-
-  #You can pass fun options to update_all, too.
-  def update_all_order_option
-    show User.update_all("admin = 1", "name LIKE 'B%'" , { :order => params[:order] })
+  Queries.each do |query|
+    class_eval <<-RUBY
+      def #{query[:action]}
+        show #{query[:query]}
+      end
+    RUBY
   end
 
   private
--- app/models/queries.rb
+++ app/models/queries.rb
@@ -1,5 +1,73 @@
 Queries = [
-  { :action => :calculate,
+  {
+    :action => :calculate,
+    :query => "Order.calculate(:sum, params[:column])",
     :input => [:name => :column, :example => "blah"]
+  },
+
+  {
+    :action => :conditions_string,
+    :query => 'User.first(:conditions => "name = \'#{params[:name]}\'")'
+  },
+
+  { :action => :exists },
+
+  {
+    :action => :from_option,
+    :query => "User.all(:from => params[:from]).where(:admin => false)"
+  },
+
+  {
+    :action => :group_option,
+    :query => "User.find(:all, :group => params[:group], :where => { :admin => false })"
+  },
+
+  {
+    :action => :having,
+    :query => 'Order.where(:customer_id => current_user).group(:customer_id).having("total > #{params[:total]}")'
+  },
+
+  {
+    :action => :having_option,
+    :query => 'Order.all(:conditions => { :customer_id => current_user }, :group => :customer_id, :having => "total > #{params[:total]}")'
+  },
+
+  {
+    :action => :joins,
+    :query => 'Order.where(:customer_id => current_user).joins(params[:table])'
+  },
+
+  {
+    :action => :lock_option
+  },
+
+  {
+    :action => :order,
+    :query => 'User.all.order("#{params[:column]} #{params[:sort]}")'
+  },
+
+  {
+    :action => :order_option,
+    :query => 'User.all(:order => "name #{params[:sort]}")'
+  },
+
+  {
+    :action => :select_option,
+    :query => 'User.first(:conditions => { :name => params[:name], :password => params[:password] }, :select => params[:column])'
+  },
+
+  {
+    :action => :where,
+    :query => 'User.all(:order => "name #{params[:sort]}")'
+  },
+
+  {
+    :action => :update_all,
+    :query => 'User.update_all(params[:update])'
+  },
+
+  {
+    :action => :update_all_order_option,
+    :query => 'User.update_all("admin = 1", "name LIKE \'B%\'" , { :order => params[:order] })'
   }
 ]
