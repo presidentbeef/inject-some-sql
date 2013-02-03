fbcc22256886ded53bb7c6f1e9ee538e5f988316
Justin Collins <justin@presidentbeef.com>
2013-02-02 18:39:43 -0800

    Syntax highlighting

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ end
 
 gem 'jquery-rails'
 
+gem 'coderay', "~> 1.0.8"
+
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
+require 'coderay'
+
 module ApplicationHelper
+  def ruby_color code
+    CodeRay.scan(code, :ruby).span.html_safe
+  end
+
+  def sql_color code
+    CodeRay.scan(code, :sql).span.html_safe
+  end
 end
