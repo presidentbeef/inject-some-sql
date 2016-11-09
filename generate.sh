a4c618d44d18cd136475084da68ac9c347d6dd0a
Justin Collins <justin@presidentbeef.com>
2016-11-09 12:12:52 -0800

    maybe rails5

--- generate.sh
+++ generate.sh
@@ -38,10 +38,29 @@ sleep 10
 wget -p http://localhost:3000/examples
 kill %1
 cp localhost\:3000/examples ../tmp/rails4.html
+cp -rf localhost\:3000/assets/* ../tmp/assets/
+rm -rf localhost\:3000
+cd ..
+
+echo "NOW RAILS 5"
+
+rvm --force default@iss gemset empty
+rvm default@iss do gem install bundler
+cd rails5
+rvm default@iss do bundle install
+rvm default@iss do bundle exec rails assets:precompile
+rvm default@iss do bundle exec rails db:reset
+rvm default@iss do bundle exec rails db:setup
+rvm default@iss do bundle exec rails s &
+sleep 10
+wget -p http://localhost:3000/examples
+kill %1
+cp localhost\:3000/examples ../tmp/rails5.html
 cp localhost\:3000/examples ../tmp/index.html
 cp -rf localhost\:3000/assets/* ../tmp/assets/
 rm -rf localhost\:3000
 cd ..
+
 
 git reset --hard
 git checkout gh-pages
@@ -49,6 +68,6 @@ rm -rf assets
 mv tmp/* .
 rm -rf tmp
 
-git add rails3.html rails4.html assets/
+git add rails3.html rails4.html git add rails5.html assets/
 echo "To deploy, add GA then:"
 echo "git add rails4.html rails3.html index.html && git commit && git push"
