f259e334efb2431957c6e323a6c3535f6f42f8a0
Justin Collins <justin.collins@gusto.com>
2019-12-30 20:32:39 -0800

    WIP

--- rails6/app/javascript/packs/application.js
+++ rails6/app/javascript/packs/application.js
@@ -7,6 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require('jquery')
+require("show_result")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
