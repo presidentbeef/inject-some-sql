bd50ae2f67cb8d6f40775485eeed2ebe30a636f4
Justin Collins <justin.collins@gusto.com>
2021-07-11 22:13:56 -0700

    Newer syntax for routes

--- rails6/config/routes.rb
+++ rails6/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  post 'query/:action', :controller => :query
+  post 'query/:action', controller: :query
 
-  get 'examples' => 'query#examples'
+  get 'examples', to: 'query#examples'
 
-  root :to => 'query#index'
+  root 'query#index'
 end
